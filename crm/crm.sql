2f079a15a5a12e374118107919e252348f148f3e
Hopcus <alyssa.hopcus@kellogg.northwestern.edu>
2023-01-17 23:37:30 +0000

    tried to run crm

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,39 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+
+DROP TABLE IF EXISTS salesperson;
+DROP TABLE IF EXISTS lead;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS company;
+
 -- CREATE TABLES
+
+
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE leads (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT,
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE activity (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contact_method TEXT,
+  date TEXT,
+  time TEXT,
+  notes TEXT
+);
